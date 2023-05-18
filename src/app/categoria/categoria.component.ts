c5d9b0bc7c54329cd36e24fbf70701ac0b550859
Nicolas Stassun <nicolas_stassun@estudante.sesisenai.org.br>
2023-05-18 16:13:52 -0300

    3.0

--- src/app/categoria/categoria.component.ts
+++ src/app/categoria/categoria.component.ts
@@ -1,23 +1,38 @@
 import { Component, OnInit } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-categoria',
   templateUrl: './categoria.component.html',
-  styleUrls: ['/categoria.component.css']
+  styleUrls: ['./categoria.component.css']
 })
-
 export class CategoriaComponent implements OnInit {
+
+  novaCategoria: string = "";
+  categorias: string[] = [];
 
   constructor() { }
 
   ngOnInit(): void {
-
-
-
+    this.pegaCategoriaDoLocalStorage();
   }
-  novaCategoria: string = "";
 
-  categorias: string [] = [];
+  cadastrarCategoria(categoria: string | null): void {
+    if (categoria !== null) {
+      this.categorias.push(categoria);
+      this.enviaCategoriaParaLocalStorage();
+      console.log(categoria);
+      this.novaCategoria = "";
+    }
+  }
 
+  pegaCategoriaDoLocalStorage(): void {
+    const listaLocalStorageCategoria = localStorage.getItem('listaDeCategorias');
+    if (listaLocalStorageCategoria !== null) {
+      this.categorias = JSON.parse(listaLocalStorageCategoria);
+    }
+  }
+
+  enviaCategoriaParaLocalStorage(): void {
+    localStorage.setItem("listaDeCategorias", JSON.stringify(this.categorias));
+  }
 }
